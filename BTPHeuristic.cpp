dfa7fe97c3ae4b9c645845465515dda88731bdef
Arnav Vohra <arnav1511995@gmail.com>
2017-04-21 14:29:02 +0530

    dacad
    asdasd

--- BTPHeuristic.cpp
+++ BTPHeuristic.cpp
@@ -15,7 +15,18 @@ class Interval
 {
 public:
 	int left, right, clientID, rangeLeft, rangeRight;
+	Interval();
 }
+
+Interval::Interval(int left, int right, int clientID, int rangeLeft, int rangeRight)
+{
+	this->left = left;
+	this->right = right;
+	this->clientID = clientID;
+	this->rangeLeft= rangeLeft;
+	this->rangeRight= rangeRight;
+}
+
 
 class Host
 {
@@ -25,9 +36,10 @@ public:
 	double t_departure=random_num(250,1000);
 	double host_rating=random_num(80,100)/100;
 	double uplink_rate=random_num(40,120), downlink_rate=(80,160);	
+	double resourceUtilization;
 	vector< Interval > taskSchedule;
 	Host();
-	void admissionControl(Host *, Client *);
+	double admissionControl(Host *, Client *);
 };
 
 Host::Host()
@@ -38,6 +50,7 @@ Host::Host()
 	host_rating=random_num(80,100)/100;
 	uplink_rate=random_num(40,120);
 	downlink_rate=random_num(80,160);
+	resourceUtilization=0;
 	taskSchedule.resize(0);
 }
 
@@ -64,94 +77,151 @@ Client::Client()
 	downlink_rate=random_num(80,160);
 }
 
-Host::admissionControl(Client *client_list, int client_ID)
+double calculateUtilization(vector<Interval> schedule)
 {
-	double utility;
+	double usedResource=0;
+	for(int i=0;i<schedule.size();i++)
+		usedResource+=schedule[i].right-schedule[i].left;
+	return schedule.size()>0? usedResource/schedule[schedule.size()-1].right : 0;
+}
 
+Host::admissionControl(Client *client_list, int client_ID, bool setFlag)
+{
+	//preprocessing of existing schedule for creating gaps to be filled by new task
 	//should use other algorithm which minimizes context switches
+	if(taskSchedule.size()==0)
+	{
+		Client client = client_list[taskSchedule[i].clientID];
+		int strt = client.t_arrival + client.task_length/ client.uplink_rate + client.task_length/downlink_rate;
+		int endd = min(client.deadline - client.results_length/uplink_rate - client.results_length/client.downlink_rate,
+		t_departure - client.results_length/client.downlink_rate);
+		if(strt>=endd || endd-strt<client.task_length/f_residual) 
+			return calculateUtilization(taskSchedule);
+		Interval interval = new Interval(endd - client.task_length/f_residual, endd, client_ID, strt, endd);
+		taskSchedule.push_back(interval);
+		return calculateUtilization(taskSchedule);
+	}
+
 	vector<Interval> rightShifted;
 	for(int i=taskSchedule.size()-1;i>=0;i--)
 	{
 		Client client = client_list[taskSchedule[i].clientID];
-		Interval interval;
-		int rightShiftAmount = i==taskSchedule.size()-1? client.deadline - taskSchedule[i].right : min(client.deadline,  rightShifted[0].left) - taskSchedule[i].right; //consider communication time later.
-		interval.right = taskSchedule[i].right + rightShiftAmount; 
-		interval.left = taskSchedule[i].left + rightShiftAmount;
-		interval.clientID = taskSchedule[i].clientID;
-		interval.rangeRight = taskSchedule[i].deadline;
-		interval.rangeLeft = taskSchedule[i].t_arrival;
+		int strt = client.t_arrival + client.task_length/ client.uplink_rate + client.task_length/downlink_rate;
+		int endd = client.deadline - client.results_length/uplink_rate - client.results_length/client.downlink_rate; 
+		int rightShiftAmount;
+		if(i==taskSchedule.size()-1)
+		{
+			rightShiftAmount = endd - taskSchedule[i].right;
+		}
+		else
+		{
+			rightShiftAmount = min(endd, rightShifted[0].left) - taskSchedule[i].right;
+		}
+		Interval interval = new Interval(taskSchedule[i].left + rightShiftAmount, 
+			taskSchedule[i].right + rightShiftAmount,
+			taskSchedule[i].clientID,
+			taskSchedule[i].t_arrival,
+			taskSchedule[i].deadline);
 		rightShifted.insert(rightShifted.begin(), interval);		
 	}
 
 	//resource allocation for newly admitted task
-	int strt = client_list[client_ID].t_arrival, endd = client_list[client_ID].deadline, ; //consider communication time
-	int gapRight, gapLeft;
-	vector<Interval> temp;
-	for(int i=rightShifted.size()-1;i>=0;i--)
+	Client client = client_list[clientID];
+	int strt = client.t_arrival+client.task_length/ client.uplink_rate+client.task_length/downlink_rate;
+	int endd = client.deadline-client.results_length/uplink_rate - client.results_length/client.downlink_rate; 
+	int gapRight=endd, gapLeft;
+	vector<Interval> gapList, newSchedule, updatedSchedule;
+	//checking if it can fit or not
+	if(endd - client.task_length/f_residual < strt)
 	{		
-		if(rightShifted[i].right> endd) continue;		
-		gapRight = endd;
+		return calculateUtilization(rightShifted);
+	}
+	//creates list of gaps where task can be filled
+	//check this
+	for(int i=rightShifted.size()-1;i>=0;i--)						
+	{		
+		if(rightShifted[i].right> endd)							
+		{
+			if(rightShifted[i].left<endd)
+				gapRight = rightShifted[i].left;
+			else 
+				gapRight = endd;
+			continue;
+		}		`   
 		gapLeft = rightShifted[i].right;
 		if(gapLeft<strt) 
-		{
-			Interval interval;
-			interval.left= end - client_list[client_ID].task_length/f_residual;
-			if(interval.left < strt) {utility = -1; return;}
-			interval.right=endd;
-			interval.clientID=client_ID;
-			interval.rangeLeft=strt;		//consider communication time
-			interval.rangeRight=endd;		//consider communication time
-			temp.push_back(interval);
-			utility=1;				//edit this
-			return;
+		{				
+			Interval interval = new Interval(endd - client.task_length/f_residual, endd, client_ID, strt, endd);		//check this
+			gapList.push_back(interval);
+			break;
 		}
-		while(i>=0 && gapLeft>strt)
+		while(i>=0 && gapRight>strt)
 		{			
-			if(gapLeft<gapRight)
-			{
-				Interval interval;
-				interval.left=max(gapLeft,strt);
-				interval.right=gapRight;
-				interval.clientID=client_ID;
-				interval.rangeLeft=strt;		//consider communication time
-				interval.rangeRight=endd;		//consider communication time
-				temp.push_back(interval);
-			}
+			Interval interval=new Interval(max(gapLeft,strt),gapRight,client_ID,strt,endd);
+			gapList.push_back(interval);
 			gapRight=rightShifted[i].left;
 			gapLeft= max(strt, (i>0? rightShifted[i-1].right : 0));
 			i--;
-		}
-		if(gapLeft==strt && gapLeft<gapRight)
-		{
-			Interval interval;
-			interval.left=max(gapLeft,strt);
-			interval.right=gapRight;
-			interval.clientID=client_ID;
-			interval.rangeLeft=strt;		//consider communication time
-			interval.rangeRight=endd;		//consider communication time
-			temp.push_back(interval);
-		}
-		
-
-		
+		}				
 	}
-	if(flag==-1)
+	//fills the gaps
+	if(gapList.size()>0)
 	{
-
-	}
-	else
-	{
-		int i=flag;
-		while(i<rightShifted.size() && rightShifted[i].left > client_list[clientID].t_arrival && rightShifted[i].right < client_list[clientID].deadline)
+		gapList.sort();
+		int execTimeLeft = client_list[client_ID].task_length/f_residual;
+		int i=gapList.size()-1;
+		while(i>=0 && execTimeLeft>0)
 		{
-			
-			i++;
+			if(gapList[i].right - gapList[i].left > execTimeLeft)
+			{
+				newSchedule.push_back(gapList[i].right - execTimeLeft, gapList[i].right);
+				execTimeLeft=0;
+			}
+			else
+			{
+				newSchedule.push_back(gapList[i].left, gapList[i].right);
+				execTimeLeft - = gapList[i].right - gapList[i].left;
+			}
+			i--;
 		}
+		if(execTimeLeft>0) 
+		{		
+			return calculateUtilization(rightShifted);
+		}
+		else 
+		{
+			updatedSchedule = rightShifted;
+			updatedSchedule.insert(updatedSchedule.end(), newSchedule.begin(), newSchedule.end());
+			updatedSchedule.sort();
+			if(setFlag==1) 
+			{
+				this->taskSchedule = updatedSchedule;
+				this->resourceUtilization=calculateUtilization(updatedSchedule);
+			}
+			return calculateUtilization(updatedSchedule);
+		}	
 	}
+	else 
+	{
+		return calculateUtilization(rightShifted);
+	}
+	
 
 }
 
-//function for schedule update after every iteration
+double calculateMeanUtilization(Host * host_list)
+{
+	double sum=0;
+	for(int i =0;i<H;i++)
+		sum+=host_list[i].resourceUtilization;
+	return sum/H;
+}
+
+void allocateResource(int clientID, Client *client_list, Host host)
+{
+	host.admissionControl(client_list, clientID, 1);
+}
+
 
 int main()
 {
@@ -167,52 +237,31 @@ int main()
 		host_revenue[i][j]= 5 + host_list[j].host_rating /40.00 * (host_list[j].f_residual/100 + client_list[i].task_length);
 	double ETT[C][H], freqs[C][H], tau[C][H], utilities[C][H];
 	for(int i=0;i<C;i++)
-	for(int j=0;j<H;j++)
 	{
-
-		// tau[i][j]=client_list[i].task_length/ client_list[i].uplink_rate +client_list[i].results_length/client_list[i].downlink_rate +
-		// 		client_list[i].task_length/ host_list[j].downlink_rate +client_list[i].results_length/host_list[j].uplink_rate + 1;
-		// freqs[i][j]=min( client_list[i].deadline , host_list[j].t_departure) - 1 - tau[i][j]>0? 
-		// (1 * client_list[i].task_length)/(min( client_list[i].deadline , host_list[j].t_departure) - 1 - tau[i][j])
-		// : 0.0001;
-		// ETT[i][j]=(client_list[i].task_length / freqs[i][j]) + tau[i][j];		
-		// utilities[i][j] = (cost_client[i]-host_revenue[i][j])*host_list[j].host_rating*(client_list[i].deadline - ETT[i][j]);
-		// cout<<cost_client[i]<<" "<<host_revenue[i][j]<<"\n";
-	}
-	ofstream outfile;
-	outfile.open("ilp.lp");
-	outfile<< "max: ";
-	for(int i=0;i<C;i++)
-	for(int j=0;j<H;j++)
-	{
-		outfile<<utilities[i][j]<<" x"<<i<<"_"<<j<<" +" ;
-	}
-	outfile<<"0;\n\n";
-	for(int i=0;i<C;i++)
-	{
+		double meanUtilization = calculateMeanUtilization(host_list);
+		double maxUtility = INT_MIN, bestHost=-1;		
 		for(int j=0;j<H;j++)
 		{
-			outfile<<"x"<<i<<"_"<<j<<" + ";
-		}
-		outfile<<"0 <= 1;\n";
-	}
 
-	for(int	j=0;j<H;j++)
-	{
-		for(int i=0;i<C;i++)
-		{
-			outfile<< freqs[i][j] <<" x"<<i<<"_"<<j<<" + ";
+			resourceUtilization = host_list[j].admissionControl(client_list, i, 0);
+
+			utilities[i][j]=(cost_client[i]-host_revenue[i][j])*host_list[j].host_rating*(1+resourceUtilization - meanUtilization);
+			if(utilities[i][j] > maxUtility)
+			{
+				maxUtility = utilities[i][j];
+				bestHost = j;
+			}
+			// tau[i][j]=client_list[i].task_length/ client_list[i].uplink_rate +client_list[i].results_length/client_list[i].downlink_rate +
+			// 		client_list[i].task_length/ host_list[j].downlink_rate +client_list[i].results_length/host_list[j].uplink_rate + 1;
+			// freqs[i][j]=min( client_list[i].deadline , host_list[j].t_departure) - 1 - tau[i][j]>0? 
+			// (1 * client_list[i].task_length)/(min( client_list[i].deadline , host_list[j].t_departure) - 1 - tau[i][j])
+			// : 0.0001;
+			// ETT[i][j]=(client_list[i].task_length / freqs[i][j]) + tau[i][j];		
+			// utilities[i][j] = (cost_client[i]-host_revenue[i][j])*host_list[j].host_rating*(client_list[i].deadline - ETT[i][j]);
+			// cout<<cost_client[i]<<" "<<host_revenue[i][j]<<"\n";
 		}
-		outfile<<"0 <= "<< host_list[j].f_residual<<";\n";
+		allocateResource(client_list[i], bestHost);
+
 	}
-	for(int i=0;i<C;i++)
-	for(int j=0;j<H;j++)
-	{
-		outfile<<"int x"<<i<<"_"<<j<<";\n";
-	}
-	outfile.close();
-	system("lp_solve ilp.lp > arnav.txt");
-	ifstream infile;
-	infile.open("arnav.txt");
 	return 0;
 }
